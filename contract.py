4737169264733f477337d25cb72272de112b4fb0
Laksh-codes <lakshay.sharma@virtuos.com>
2025-09-08 12:03:11 +0530

    Update contract.py

--- contract.py
+++ contract.py
@@ -24,20 +24,15 @@ def parser(whole_data):
 count = 0
 for i in range(len(final)):
     print(count)
-    url1 = f"https://asiapacificbreweriestest.agiloft.com/ewws/EWSearch?$KB=Asia Pacific Breweries Test&$table=contract&$login=Admin1&$password=Integration@2025&$lang=en&query=id={final[i]}&field=wfstate&field=record_type&field=contract_type&field=contract_start_date&field=contract_end_date&field=contract_term_in_months&field=addendum_effective_date&field=new_sub_category"
+    url1 = f""
 
     responce1 = requests.request("Get",url1)
     data1 = responce1.text
     whole_data.update({final[i]:data1.replace("\r", "").replace("\n", "").split(';')})
     count += 1
 # print(whole_data)
-pattern = re.compile(r"(?:ST_|EWREST_)?(\w+?)(?:_0)?\s*=\s*'([^']*)'")
+pattern = re.compile(r"")
 dictonary = parser(whole_data)
 print("JSON Parsed.")
 print(dictonary)
 
-#saved search - name given to preset filters to display subset of universe
-#example = we have 2 types of contracts. BSA and BIP
-#If the saved search is created with name 'active BSA' then it will only return 
-# contract document type - BSA
-# status = Active
